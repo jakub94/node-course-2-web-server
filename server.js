6b7f5d36975253522c6df52ff048d2e51aa6bf5b
Jakub <jakub94@gmx.de>
2018-04-23 22:42:33 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000; // defaults to 3000 if PORT environment variable does not exist. (It does exist on Heroku machines)
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials"); //Tells Handlebars where to find its partials
@@ -77,7 +78,6 @@ app.get("/bad", (req, res) => {
 });
 
 
-
-app.listen(3000, () => {
-  console.log("Server is up on port: ", 3000);
+app.listen(port, () => {
+  console.log("Server is up on port: ", port);
 });
